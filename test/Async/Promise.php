67018535bd68960a7323e293b4293e0ca34c1551
aprilsacil <aprilvsacil@gmail.com>
2019-03-14 11:46:46 +0800

    reenabled commented test

--- test/Async/Promise.php
+++ test/Async/Promise.php
@@ -34,199 +34,199 @@ class Cradle_Async_Promise_Test extends TestCase
     {
     }
 
-    // /**
-    //  * @covers Cradle\Async\Promise::then
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testThen()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //     $promise = Promise::i(function($fulfill, $reject) {
-    //         $fulfill(1);
-    //     }, $handler);
-    //
-    //     $test = $this;
-    //     $called = false;
-    //     $promise
-    //         ->then(function ($value) {
-    //             return $value + 1;
-    //         })
-    //         ->then(function ($value) use ($test, &$called) {
-    //             $called = true;
-    //             $test->assertEquals(2, $value);
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertTrue($called);
-    //
-    //     // test error
-    //     $item = 0;
-    //     $error = false;
-    //     $promise1 = Promise::i(function($fulfill) {
-    //         // this is a typo and should result to an error
-    //         $fullfill(10);
-    //     }, $handler);
-    //
-    //     $called = false;
-    //     $promise1
-    //         ->then(function ($value) use (&$item) {
-    //             $item = $value;
-    //         })
-    //         ->catch(function($err) use (&$error) {
-    //             $error =  true;
-    //         });
-    //
-    //     $handler->run();
-    //
-    //     $this->assertEquals(0, $item);
-    //     $this->assertTrue($error);
-    // }
-    //
-    // /**
-    //  * @covers Cradle\Async\Promise::catch
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testCatch()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //     $promise = Promise::i(function($fulfill, $reject) {
-    //         $reject(1);
-    //     }, $handler);
-    //
-    //     $test = $this;
-    //     $called = false;
-    //     $promise
-    //         ->catch(function ($value) {
-    //             return $value + 1;
-    //         })
-    //         ->catch(function ($value) use ($test, &$called) {
-    //             $called = true;
-    //             $test->assertEquals(2, $value);
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertTrue($called);
-    // }
-    //
-    // /**
-    //  * @covers Cradle\Async\Promise::then
-    //  * @covers Cradle\Async\Promise::catch
-    //  * @covers Cradle\Async\Promise::finally
-    //  * @covers Cradle\Async\Promise::getState
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testFinally()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //     $promise = Promise::i(function($fulfill, $reject) {
-    //         $fulfill(1);
-    //     }, $handler);
-    //
-    //     $test = $this;
-    //     $called = 0;
-    //
-    //     $promise
-    //         ->then(function ($x) {
-    //             return $x + 1;
-    //         })
-    //         ->then(function ($x) {
-    //             return $x + 1;
-    //         })
-    //         ->finally(function () use (&$promise, &$test, &$called) {
-    //             $called ++;
-    //             $test->assertEquals(1, $promise->getState());
-    //         })
-    //         ->then(function ($x) use (&$promise, &$test, &$called) {
-    //             $called ++;
-    //             $test->assertEquals(3, $x);
-    //             throw new Exception('fail');
-    //         })
-    //         ->catch(function ($message) use (&$promise, &$test, &$called) {
-    //             $called ++;
-    //             $test->assertEquals(2, $promise->getState());
-    //
-    //             // overwrite state
-    //             $test->assertEquals(3, $promise->getState(3));
-    //             $test->assertEquals('fail', $message);
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertEquals(3, $called);
-    //
-    //     // test finally without triggering fullfill
-    //     $called = 0;
-    //     $promise1 = Promise::i(function($fulfill, $reject) use (&$called) {
-    //         $called += 1;
-    //     }, $handler);
-    //
-    //     $promise
-    //         ->finally(function () use (&$called) {
-    //             $called++;
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertEquals(2, $called);
-    // }
-    //
-    // /**
-    //  * @covers Cradle\Async\Promise::reject
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::then
-    //  * @covers Cradle\Async\Promise::catch
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testReject()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //
-    //     $test = $this;
-    //     $called = false;
-    //     Promise::reject(1, $handler)
-    //         ->then(null, function ($value) {
-    //             return $value + 1;
-    //         })
-    //         ->catch(function ($value) use ($test, &$called) {
-    //             $called = true;
-    //             $test->assertEquals(2, $value);
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertTrue($called);
-    // }
-    //
-    // /**
-    //  * @covers Cradle\Async\Promise::resolve
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::then
-    //  * @covers Cradle\Async\Promise::catch
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testResolve()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //
-    //     $test = $this;
-    //     $called = false;
-    //     Promise::resolve(1, $handler)
-    //         ->then(function ($value) {
-    //             return $value + 1;
-    //         })
-    //         ->then(function ($value) use ($test, &$called) {
-    //             $called = true;
-    //             $test->assertEquals(2, $value);
-    //         });
-    //
-    //     $handler->run();
-    //     $this->assertTrue($called);
-    // }
+    /**
+     * @covers Cradle\Async\Promise::then
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testThen()
+    {
+        $handler = new AsyncHandler('noop');
+        $promise = Promise::i(function($fulfill, $reject) {
+            $fulfill(1);
+        }, $handler);
+
+        $test = $this;
+        $called = false;
+        $promise
+            ->then(function ($value) {
+                return $value + 1;
+            })
+            ->then(function ($value) use ($test, &$called) {
+                $called = true;
+                $test->assertEquals(2, $value);
+            });
+
+        $handler->run();
+        $this->assertTrue($called);
+
+        // test error
+        $item = 0;
+        $error = false;
+        $promise1 = Promise::i(function($fulfill) {
+            // this is a typo and should result to an error
+            $fullfill(10);
+        }, $handler);
+
+        $called = false;
+        $promise1
+            ->then(function ($value) use (&$item) {
+                $item = $value;
+            })
+            ->catch(function($err) use (&$error) {
+                $error =  true;
+            });
+
+        $handler->run();
+
+        $this->assertEquals(0, $item);
+        $this->assertTrue($error);
+    }
+
+    /**
+     * @covers Cradle\Async\Promise::catch
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testCatch()
+    {
+        $handler = new AsyncHandler('noop');
+        $promise = Promise::i(function($fulfill, $reject) {
+            $reject(1);
+        }, $handler);
+
+        $test = $this;
+        $called = false;
+        $promise
+            ->catch(function ($value) {
+                return $value + 1;
+            })
+            ->catch(function ($value) use ($test, &$called) {
+                $called = true;
+                $test->assertEquals(2, $value);
+            });
+
+        $handler->run();
+        $this->assertTrue($called);
+    }
+
+    /**
+     * @covers Cradle\Async\Promise::then
+     * @covers Cradle\Async\Promise::catch
+     * @covers Cradle\Async\Promise::finally
+     * @covers Cradle\Async\Promise::getState
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testFinally()
+    {
+        $handler = new AsyncHandler('noop');
+        $promise = Promise::i(function($fulfill, $reject) {
+            $fulfill(1);
+        }, $handler);
+
+        $test = $this;
+        $called = 0;
+
+        $promise
+            ->then(function ($x) {
+                return $x + 1;
+            })
+            ->then(function ($x) {
+                return $x + 1;
+            })
+            ->finally(function () use (&$promise, &$test, &$called) {
+                $called ++;
+                $test->assertEquals(1, $promise->getState());
+            })
+            ->then(function ($x) use (&$promise, &$test, &$called) {
+                $called ++;
+                $test->assertEquals(3, $x);
+                throw new Exception('fail');
+            })
+            ->catch(function ($message) use (&$promise, &$test, &$called) {
+                $called ++;
+                $test->assertEquals(2, $promise->getState());
+
+                // overwrite state
+                $test->assertEquals(3, $promise->getState(3));
+                $test->assertEquals('fail', $message);
+            });
+
+        $handler->run();
+        $this->assertEquals(3, $called);
+
+        // test finally without triggering fullfill
+        $called = 0;
+        $promise1 = Promise::i(function($fulfill, $reject) use (&$called) {
+            $called += 1;
+        }, $handler);
+
+        $promise
+            ->finally(function () use (&$called) {
+                $called++;
+            });
+
+        $handler->run();
+        $this->assertEquals(2, $called);
+    }
+
+    /**
+     * @covers Cradle\Async\Promise::reject
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::then
+     * @covers Cradle\Async\Promise::catch
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testReject()
+    {
+        $handler = new AsyncHandler('noop');
+
+        $test = $this;
+        $called = false;
+        Promise::reject(1, $handler)
+            ->then(null, function ($value) {
+                return $value + 1;
+            })
+            ->catch(function ($value) use ($test, &$called) {
+                $called = true;
+                $test->assertEquals(2, $value);
+            });
+
+        $handler->run();
+        $this->assertTrue($called);
+    }
+
+    /**
+     * @covers Cradle\Async\Promise::resolve
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::then
+     * @covers Cradle\Async\Promise::catch
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testResolve()
+    {
+        $handler = new AsyncHandler('noop');
+
+        $test = $this;
+        $called = false;
+        Promise::resolve(1, $handler)
+            ->then(function ($value) {
+                return $value + 1;
+            })
+            ->then(function ($value) use ($test, &$called) {
+                $called = true;
+                $test->assertEquals(2, $value);
+            });
+
+        $handler->run();
+        $this->assertTrue($called);
+    }
 
     /**
      * @covers Cradle\Async\Promise::all
@@ -385,100 +385,100 @@ class Cradle_Async_Promise_Test extends TestCase
         $test->assertFalse($called);
     }
 
-    // /**
-    //  * @covers Cradle\Async\Promise::race
-    //  * @covers Cradle\Async\Promise::__construct
-    //  * @covers Cradle\Async\Promise::getCoroutine
-    //  * @covers Cradle\Async\Promise::then
-    //  * @covers Cradle\Async\Promise::catch
-    //  * @covers Cradle\Async\Promise::settle
-    //  */
-    // public function testRace()
-    // {
-    //     $handler = new AsyncHandler('noop');
-    //
-    //     $promise1 = new Promise(function($fulfill) {
-    //         for($i = 0; $i < 10; $i++) {
-    //             yield $i;
-    //         }
-    //
-    //         $fulfill($i);
-    //     }, $handler);
-    //
-    //     $promise2 = new Promise(function($fulfill) {
-    //         for($i = 0; $i < 5; $i++) {
-    //             yield $i;
-    //         }
-    //
-    //         $fulfill($i);
-    //     }, $handler);
-    //
-    //     $called = false;
-    //     $error = null;
-    //     $test = $this;
-    //
-    //     Promise::race([$promise1, $promise2], $handler)->then(function($value) use (&$called, $test) {
-    //         $called = true;
-    //         $test->assertEquals(5, $value);
-    //     })->catch(function($err) use (&$error) {
-    //         $error = $err;
-    //     });
-    //
-    //     $handler->run();
-    //     $this->assertNull($error);
-    //     $this->assertTrue($called);
-    //
-    //     // Test Rejections
-    //     $promise3 = new Promise(function($fullfill, $reject) {
-    //         $reject('yes');
-    //     }, $handler);
-    //
-    //     $promise4 = new Promise(function($fulfill, $reject) {
-    //         for($i = 0; $i <= 10; $i++) {
-    //             yield $i;
-    //         }
-    //
-    //         $reject($i + 1);
-    //     }, $handler);
-    //
-    //
-    //     $called = false;
-    //     Promise::race([$promise3, $promise4], $handler)->then(function($value) use (&$called, $test) {
-    //         $called = true;
-    //         $test->assertEquals(8, $value);
-    //     })->catch(function($err) use (&$error) {
-    //         $error = $err;
-    //     });
-    //
-    //     $handler->run(function($value) {
-    //     });
-    //
-    //     $test->assertNotEmpty($error);
-    //     $test->assertFalse($called);
-    //
-    //     // test not promise
-    //     $promise5 = 500;
-    //     $promise6 = new Promise(function($fulfill) {
-    //         for($i = 0; $i < 5; $i++) {
-    //             yield $i;
-    //         }
-    //
-    //         $fulfill($i);
-    //     }, $handler);
-    //
-    //     $called = false;
-    //     $error = null;
-    //     Promise::race([$promise5, $promise6], $handler)->then(function($value) use (&$called, $test) {
-    //         $called = true;
-    //         $test->assertEquals(500, $value);
-    //     })->catch(function($err) use (&$error) {
-    //         $error = $err;
-    //     });
-    //
-    //     $handler->run(function($value) {
-    //     });
-    //
-    //     $test->assertNull($error);
-    //     $test->assertTrue($called);
-    // }
+    /**
+     * @covers Cradle\Async\Promise::race
+     * @covers Cradle\Async\Promise::__construct
+     * @covers Cradle\Async\Promise::getCoroutine
+     * @covers Cradle\Async\Promise::then
+     * @covers Cradle\Async\Promise::catch
+     * @covers Cradle\Async\Promise::settle
+     */
+    public function testRace()
+    {
+        $handler = new AsyncHandler('noop');
+
+        $promise1 = new Promise(function($fulfill) {
+            for($i = 0; $i < 10; $i++) {
+                yield $i;
+            }
+
+            $fulfill($i);
+        }, $handler);
+
+        $promise2 = new Promise(function($fulfill) {
+            for($i = 0; $i < 5; $i++) {
+                yield $i;
+            }
+
+            $fulfill($i);
+        }, $handler);
+
+        $called = false;
+        $error = null;
+        $test = $this;
+
+        Promise::race([$promise1, $promise2], $handler)->then(function($value) use (&$called, $test) {
+            $called = true;
+            $test->assertEquals(5, $value);
+        })->catch(function($err) use (&$error) {
+            $error = $err;
+        });
+
+        $handler->run();
+        $this->assertNull($error);
+        $this->assertTrue($called);
+
+        // Test Rejections
+        $promise3 = new Promise(function($fullfill, $reject) {
+            $reject('yes');
+        }, $handler);
+
+        $promise4 = new Promise(function($fulfill, $reject) {
+            for($i = 0; $i <= 10; $i++) {
+                yield $i;
+            }
+
+            $reject($i + 1);
+        }, $handler);
+
+
+        $called = false;
+        Promise::race([$promise3, $promise4], $handler)->then(function($value) use (&$called, $test) {
+            $called = true;
+            $test->assertEquals(8, $value);
+        })->catch(function($err) use (&$error) {
+            $error = $err;
+        });
+
+        $handler->run(function($value) {
+        });
+
+        $test->assertNotEmpty($error);
+        $test->assertFalse($called);
+
+        // test not promise
+        $promise5 = 500;
+        $promise6 = new Promise(function($fulfill) {
+            for($i = 0; $i < 5; $i++) {
+                yield $i;
+            }
+
+            $fulfill($i);
+        }, $handler);
+
+        $called = false;
+        $error = null;
+        Promise::race([$promise5, $promise6], $handler)->then(function($value) use (&$called, $test) {
+            $called = true;
+            $test->assertEquals(500, $value);
+        })->catch(function($err) use (&$error) {
+            $error = $err;
+        });
+
+        $handler->run(function($value) {
+        });
+
+        $test->assertNull($error);
+        $test->assertTrue($called);
+    }
 }
